463015348efe41e50ab5194c6ad91e959977d3e4
francecil <396324491@qq.com>
2023-04-05 16:39:31 +0800

    feat: 完成主题基础构建 (+6 squashed commits) Squashed commits: [7856f4c] feat: 添加 vdoing 源码方便修改 [82e6686] refactor: blog 目录 [f0d9995] refactor: 调整随笔目录 [3a8634c] refactor: 目录迁移 [3162f16] refactor: remove next [43dcb20] ignore lock

--- source/categories/index.md
+++
@@ -1,6 +0,0 @@
----
-title: categories
-date: 2019-12-11 19:57:08
-type: "categories"
-comments: false
----
---
+++ vdoing/node_utils/modules/readFileList.js
@@ -0,0 +1,43 @@
+/**
+ *  读取所有md文件数据
+ */
+const fs = require('fs'); // 文件模块
+const path = require('path'); // 路径模块
+const chalk = require('chalk') // 命令行打印美化
+const log = console.log;
+const { globSync } = require('glob');
+
+/**
+ * 获取 blog 文件列表
+ * @param {*} dir 
+ * @param {*} patterns 
+ * @returns 
+ */
+function readFileList(dir, patterns = ['**/*.md', '**/*.vue']) {
+  const files = globSync(patterns, { cwd: dir, ignore: ['node_modules/**', '.vuepress/**', '@pages/**'] }).map((_filePath) => {
+    const filePath = path.join(dir, _filePath)
+    const filename = path.basename(filePath)
+    const fileNameArr = filename.split('.')
+
+    if (fileNameArr.length <= 2) {
+      return {
+        name: fileNameArr[0],
+        type: fileNameArr[1],
+        filePath
+      }
+    }
+    const fileType = fileNameArr.pop()
+    // 序号开头的文件
+    if (/^\d+$/.test(fileNameArr[0])) {
+      fileNameArr.shift();
+    }
+    return {
+      name: fileNameArr.join('.'),
+      type: fileType,
+      filePath,
+    }
+  })
+  return files
+}
+
+module.exports = readFileList;
