06d59ba8159f591e2b4406789cda2155bb74a507
francecil <396324491@qq.com>
2023-06-06 20:12:29 +0800

    article: 增加技术写作相关文章

--- vdoing/util/postData.js
+++ vdoing/util/postData.js
@@ -8,7 +8,7 @@ export function filterPosts(posts) {
   posts = posts.filter(item => {
     const { frontmatter: { pageComponent, article, home, titleTag } } = item
     // 过滤存在页面组件、非文章页 (article 字段为false)、首页 、非专题页、草稿页
-    return !(pageComponent || article === false || home === true || titleTag === '专题' || titleTag === '草稿')
+    return !(pageComponent || article === false || home === true || ['专题', '草稿', '卡片'].includes(titleTag))
   })
   return posts
 }
